952dbf70f8861b3a3a92964d24ac2798d8e4e9c8
Paulo Marcos <paulo.marcos@zmp.co.jp>
2021-08-05 19:24:29 +0900

    update date 2

--- 1ya/js/main.js
+++ 1ya/js/main.js
@@ -252,7 +252,7 @@
 	
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Aug 05, 2021 19:23:00").getTime();
+		var countDownDate = new Date("Aug 05, 2021 19:40:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
